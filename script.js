711f53be49bdd85757733df69a4d36be6f471319
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-02-10 12:28:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const dateObj1 = new Date(date1);
+const dateObj2 = new Date(date2);
+const diff = dateObj2 - dateObj1;
+return Math.floor(diff/86400000);
 };
 
 // Do not change the code below.
